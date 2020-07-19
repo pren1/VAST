34fb57ab2e8f1b9a3161f459db00d6db249410b5
ScilientHill <pren1@ucsc.edu>
2020-07-19 15:14:33 -0700

    test for the 4 people

--- build_target_dict.py
+++ build_target_dict.py
@@ -14,39 +14,38 @@ def read_target_csv(path_to_csv):
 		dataset = [row[0] for row in reader]
 	return np.asarray(dataset)
 
-target_path = "target_link.csv"
-df = pd.read_csv(target_path, header=None)
+# target_path = "target_link.csv"
+# df = pd.read_csv(target_path, header=None)
+#
+# aug_df = pd.read_csv("aug_target_link.csv", header=None)
+# aug_output_dict = {}
+#
+# output_dict = {}
+# for index in range(len(df)):
+# 	line = df.loc[index]
+# 	output_dict[line[0]] = line[1:].tolist()
+#
+# 	aug_line = aug_df.loc[index]
+# 	if index == 0:
+# 		aug_output_dict[line[0]] = aug_line[1:].tolist()
+# 	else:
+# 		aug_output_dict[line[0]] = aug_line[1:-1].tolist()
+#
+# save_data_array_as_npy(aug_output_dict, "aug_multiple_name_dict")
 
-aug_df = pd.read_csv("aug_target_link.csv", header=None)
-aug_output_dict = {}
-
-output_dict = {}
-for index in range(len(df)):
-	line = df.loc[index]
-	output_dict[line[0]] = line[1:].tolist()
-
-	aug_line = aug_df.loc[index]
-	if index == 0:
-		aug_output_dict[line[0]] = aug_line[1:].tolist()
-	else:
-		aug_output_dict[line[0]] = aug_line[1:-1].tolist()
-
-save_data_array_as_npy(aug_output_dict, "aug_multiple_name_dict")
-
-# output_dict = {'宝鐘マリン': [],
-#                '白上フブキ': [],
-#                '天音かなた': [],
-#                '夏色まつり': []}
+output_dict = {'宝钟玛琳': [],
+               '白上吹雪': [],
+               '天音彼方': [],
+               '夏色祭': []}
 #
 # output_dict = {'白上フブキ': [],
 #                '夏色まつり': []}
 #
-# root_path = 'Name_CSV/'
-#
-# for key in output_dict:
-# 	path = root_path + key + ".csv"
-# 	result = read_target_csv(path)
-# 	output_dict[key] = result
-#
-# save_data_array_as_npy(output_dict, "file_name_dict")
+root_path = 'Name_CSV/'
+
+for key in output_dict:
+	path = root_path + key + ".csv"
+	result = read_target_csv(path)
+	output_dict[key] = result
+save_data_array_as_npy(output_dict, "file_name_dict")
 
